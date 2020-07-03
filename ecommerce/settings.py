8416504d9f353b590ddd30967fab9e0643235986
Unyime Etim <lordunyime@gmail.com>
2020-07-03 16:23:57 +0100

    added crispy forms

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -46,7 +46,10 @@ INSTALLED_APPS = [
      #installed app
     'django_filters',
     'storages',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
